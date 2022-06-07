1771feb9b75fa5d4291e815dc0f3434bb00dd8e0
jsonvas <ysonvas@gmail.com>
2022-06-06 19:20:51 -0500

    added: pwa support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,43 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+	plugins: [
+		VitePWA({
+			includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+			manifest: {
+				name: 'Chat Group',
+				short_name: 'Chat Group',
+				description: 'A web chat to use with your friends',
+				theme_color: '#ffffff',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable',
+					},
+				],
+			},
+			workbox: {
+				cleanupOutdatedCaches: true,
+				skipWaiting: true,
+				clientsClaim: true,
+			},
+			registerType: 'prompt',
+		}),
+		vue(),
+	],
 })
